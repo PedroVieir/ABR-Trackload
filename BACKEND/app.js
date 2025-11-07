65bd58c2916439da0e99928ede02645171e81603
Igor Amaral <igoramaraal089@gmail.com>
2025-11-07 09:11:44 -0300

    Fix: Corrigindo o caminho para acesso nas apis

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/upload', uploadRoutes);
-app.use('/documentos', documentoRoutes);
+app.use('/api/upload', uploadRoutes);
+app.use('/api/documentos', documentoRoutes);
 
 module.exports = app;
